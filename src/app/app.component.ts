ea49974fb88c52645a2a2027bc622aed628e5195
RumonHasan <rumonuniv@gmail.com>
2023-12-24 19:49:14 +0900

    extra changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component, ElementRef, HostListener, Inject } from '@angular/core';
 import { Task } from './task/task';
-import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { Store } from '@ngrx/store';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 // import { AngularFireDatabase } from '@angular/fire/compat/database';
 // import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl } from '@angular/forms';
-import { activateAllView, activateCategoryView, 
-  controlThemeState, filterTasks, passTasksByCategories, turnToDefaultView, uploadDoneTasks } from './state/tasks.actions';
+import {
+  activateAllView,
+  activateCategoryView,
+  controlThemeState,
+  filterTasks,
+  passTasksByCategories,
+  turnToDefaultView,
+  uploadDoneTasks,
+} from './state/tasks.actions';
 import { Dialog } from '@angular/cdk/dialog';
 import { QuickSearchComponentComponent } from './quick-search-component/quick-search-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-firebase-todo';
@@ -26,7 +42,7 @@ export class AppComponent {
   searchTerm = new FormControl('');
   selectedSearchCategory!: any;
   allSearch: string = 'All';
-  // search categories: 
+  // search categories:
   categories: any = [];
   // filter state
   localFilterState: boolean = false;
@@ -44,7 +60,7 @@ export class AppComponent {
   // inprogress list and done list
   doneList: any = [];
   inProgressList: any = [];
-  // quick search 
+  // quick search
   private searchDialogTriggerEl!: ElementRef;
 
   constructor(
@@ -52,152 +68,184 @@ export class AppComponent {
     private quickSearchDialog: MatDialog,
     // private quickSearchDialogRef: MatDialogRef<QuickSearchComponentComponent>,
     // getting the initial state from the task reducer
-    private taskStore: Store<{taskReducer: {tasks: Task[], categories: [], 
-      filterState: boolean, filteredTasks: Task[], themeState: boolean, 
-      // in progress and done tasks
-      doneTasks:[], inprogressTasks:[],
-      categoryTasks: object, views:any, categoryView: boolean, allView: boolean}}>,
-    private overlayContainer: OverlayContainer,
-    ){}
+    private taskStore: Store<{
+      taskReducer: {
+        tasks: Task[];
+        categories: [];
+        filterState: boolean;
+        filteredTasks: Task[];
+        themeState: boolean;
+        // in progress and done tasks
+        doneTasks: [];
+        inprogressTasks: [];
+        categoryTasks: object;
+        views: any;
+        categoryView: boolean;
+        allView: boolean;
+      };
+    }>,
+    private overlayContainer: OverlayContainer
+  ) {}
   //function to open a dialog box for adding a new todo
 
-  ngOnInit():void{
+  ngOnInit(): void {
     // getting data from store
-    this.taskStore.select('taskReducer').subscribe((data)=> 
-      this.todos = data.tasks
-    );
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.todos = data.tasks));
     // upload and fetch done tasks
     this.taskStore.dispatch(uploadDoneTasks());
-    this.taskStore.select('taskReducer').subscribe((data)=>{
+    this.taskStore.select('taskReducer').subscribe((data) => {
       this.doneList = data.doneTasks;
-    })
+    });
 
     // populating categories and creating a new set from array
-    this.taskStore.select('taskReducer').subscribe((data)=>
-      this.categories = [...new Set([...data.categories])]
-    );
+    this.taskStore
+      .select('taskReducer')
+      .subscribe(
+        (data) => (this.categories = [...new Set([...data.categories])])
+      );
     // getting the filtered state
-    this.taskStore.select('taskReducer').subscribe((data)=>
-      this.localFilterState = data.filterState
-    )
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.localFilterState = data.filterState));
     // filtered tasks
-    this.taskStore.select('taskReducer').subscribe((data)=>
-      this.filteredTodos = data.filteredTasks
-    );
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.filteredTodos = data.filteredTasks));
     // getting the theme state
-    this.taskStore.select('taskReducer').subscribe((data)=>
-      this.themeStateLocal = data.themeState
-    );
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.themeStateLocal = data.themeState));
     // passing the categories and tasks for distribution... autorefresh
-    this.taskStore.dispatch(passTasksByCategories({categories: this.categories, 
-      tasks: this.todos}));
-      // get the data from category based on tasks
-    this.taskStore.select('taskReducer').subscribe((data)=>
-        this.categoricalTasks = data.categoryTasks
+    this.taskStore.dispatch(
+      passTasksByCategories({ categories: this.categories, tasks: this.todos })
     );
+    // get the data from category based on tasks
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.categoricalTasks = data.categoryTasks));
     // get the views
-    this.taskStore.select('taskReducer').subscribe((data)=>
-      this.views = data.views
-    );
+    this.taskStore
+      .select('taskReducer')
+      .subscribe((data) => (this.views = data.views));
     // category views
-    this.taskStore.select('taskReducer').subscribe((data)=>{
+    this.taskStore.select('taskReducer').subscribe((data) => {
       this.localCategoryView = data.categoryView;
       this.localAllView = data.allView;
-    }
-    );
+    });
   }
 
   // drag and drop controls
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
 
-  // open dialog 
-  openDialog(){
-    this.dialog.open(TaskDialogComponent,{
+  // open dialog
+  openDialog() {
+    this.dialog.open(TaskDialogComponent, {
       // passing some dimensions
       width: '450px',
       height: '450px',
-      data: 'right click'
+      data: 'right click',
     });
-  };
+  }
   // search and filter tasks
-  searchTasks(){
+  searchTasks() {
     const searchTerm = this.searchTerm.getRawValue();
     // custom search and switching back to default list
-    if(searchTerm?.toLowerCase() === this.allSearch.toLowerCase()){
-      this.taskStore.dispatch(filterTasks({searchTerm: searchTerm, filterState: this.localFilterState}));
+    if (searchTerm?.toLowerCase() === this.allSearch.toLowerCase()) {
+      this.taskStore.dispatch(
+        filterTasks({
+          searchTerm: searchTerm,
+          filterState: this.localFilterState,
+        })
+      );
       this.taskStore.dispatch(turnToDefaultView());
-    }else{
-      this.taskStore.dispatch(filterTasks({searchTerm: searchTerm, filterState: this.localFilterState}));
+    } else {
+      this.taskStore.dispatch(
+        filterTasks({
+          searchTerm: searchTerm,
+          filterState: this.localFilterState,
+        })
+      );
       this.taskStore.dispatch(turnToDefaultView());
     }
-  };
+  }
   // theme toggle switch function
-  toggleTheme(){
+  toggleTheme() {
     this.taskStore.dispatch(controlThemeState());
-  };
+  }
 
   // overlay switch by fetching the class from toggle theme
-  themeChange(){
-   this.toggleTheme();
-   if(this.themeStateLocal){
-    this.overlayContainer.getContainerElement().classList.remove('dark-theme');
-    this.overlayContainer.getContainerElement().classList.add('light-theme');
-   }else{
-    this.overlayContainer.getContainerElement().classList.remove('light-theme');
-    this.overlayContainer.getContainerElement().classList.add('dark-theme');
-   }
+  themeChange() {
+    this.toggleTheme();
+    if (this.themeStateLocal) {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('dark-theme');
+      this.overlayContainer.getContainerElement().classList.add('light-theme');
+    } else {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('light-theme');
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    }
   }
   // get changes in view
-  changeView(viewStateValue: any){
+  changeView(viewStateValue: any) {
     const selectValue = viewStateValue.source.value;
-    if(selectValue.toLowerCase() === 'categoryview'){
+    if (selectValue.toLowerCase() === 'categoryview') {
       this.taskStore.dispatch(activateCategoryView());
     }
-    if(selectValue.toLowerCase() === 'all'){
+    if (selectValue.toLowerCase() === 'all') {
       this.taskStore.dispatch(turnToDefaultView());
-    } 
+    }
   }
 
   // quick search dialog control
 
   // setting dimensions
-  setMatDialogDimenions(){
+  setMatDialogDimenions() {
     const defaultPosition = {
       top: '40px',
-      bottom: '0px'
-    }
+      bottom: '0px',
+    };
     return defaultPosition;
   }
   // quick search features
-  openQuickSearchDialog(){
-    this.quickSearchDialog.open(QuickSearchComponentComponent,
-      {
-        width: '500px',
-        height: '90px',
-        position: this.setMatDialogDimenions(),
-      }  
-    )
-  };
+  openQuickSearchDialog() {
+    this.quickSearchDialog.open(QuickSearchComponentComponent, {
+      width: '500px',
+      height: '90px',
+      position: this.setMatDialogDimenions(),
+    });
+  }
   // application host listener for detecting keypress
   @HostListener('document:keypress', ['$event'])
-  handleKeyBoardEvent(keyPressEvent: KeyboardEvent){
+  handleKeyBoardEvent(keyPressEvent: KeyboardEvent) {
     const searchbarKey = {
       ctrlKey: keyPressEvent.ctrlKey,
       commandKey: keyPressEvent.metaKey,
-      key: 'k'
+      key: 'k',
     };
-    if((searchbarKey.ctrlKey || searchbarKey.commandKey) && searchbarKey.key.toLowerCase() === 'k'){
+    if (
+      (searchbarKey.ctrlKey || searchbarKey.commandKey) &&
+      searchbarKey.key.toLowerCase() === 'k'
+    ) {
       this.openQuickSearchDialog();
     }
   }
--- src/app/state/tasks.reducer.ts
+++ src/app/state/tasks.reducer.ts
@@ -1,242 +1,261 @@
-import { createReducer, on } from "@ngrx/store";
-import { initialStates } from "./tasks.state";
-import { getTasks, addNewTask, deleteTask, editTaskDialogState, editTask, 
-    getCommentId, addCommentToTask, deleteComment, 
-    toggleArchiveState, deleteCategorySearch, 
-    filterTasks, switchFilterState, controlThemeState,
-     passTasksByCategories, getViews, activateCategoryView, 
-     activateAllView, turnToDefaultView, uploadDoneTasks } from "./tasks.actions";
+import { createReducer, on } from '@ngrx/store';
+import { initialStates } from './tasks.state';
+import {
+  getTasks,
+  addNewTask,
+  deleteTask,
+  editTaskDialogState,
+  editTask,
+  getCommentId,
+  addCommentToTask,
+  deleteComment,
+  toggleArchiveState,
+  deleteCategorySearch,
+  filterTasks,
+  switchFilterState,
+  controlThemeState,
+  passTasksByCategories,
+  getViews,
+  activateCategoryView,
+  activateAllView,
+  turnToDefaultView,
+  uploadDoneTasks,
+} from './tasks.actions';
 
 // main task reducer
-const task_reducer = createReducer(initialStates, 
-    // fetching the initial tasks
-    on(getTasks, (state: any)=>{
-        return {
-            ...state,
-        }
-    }),
-    // adding a new task
-    on(addNewTask, (state: any, action)=>{
-        return {
-            ...state,
-            tasks: [...state.tasks, action.newTask],
-            categories: [...state.categories, action.newTask.category]
-        }
-    }),
-    // deleting a task including the category
-    on(deleteTask, (state: any, action)=>{
-        const possibleDeleteCategory = action.deleteCategory; // temporary
-        const tasksAfterDeleting = state.tasks.filter((singleTask: any)=> 
-        singleTask.id !== action.deleteId);
-        return {
-            ...state,
-            tasks: tasksAfterDeleting
-        }
-    }),
+const task_reducer = createReducer(
+  initialStates,
+  // fetching the initial tasks
+  on(getTasks, (state: any) => {
+    return {
+      ...state,
+    };
+  }),
+  // adding a new task
+  on(addNewTask, (state: any, action) => {
+    return {
+      ...state,
+      tasks: [...state.tasks, action.newTask],
+      categories: [...state.categories, action.newTask.category],
+    };
+  }),
+  // deleting a task including the category
+  on(deleteTask, (state: any, action) => {
+    const possibleDeleteCategory = action.deleteCategory; // temporary
+    const tasksAfterDeleting = state.tasks.filter(
+      (singleTask: any) => singleTask.id !== action.deleteId
+    );
+    return {
+      ...state,
+      tasks: tasksAfterDeleting,
+    };
+  }),
 
-    // refresh category after deleting task... note runs after deletion function
-    on(deleteCategorySearch, (state: any, action)=>{
-        const category = action.deleteCategory;
-        // check for remaining deletion categories
-        const checkRemaining = state.tasks.some((task: any)=> 
-            task.category.toLowerCase() === category.toLowerCase()
-        );
-        let newCategories = [];
-        if(!checkRemaining){
-            newCategories = state.categories.filter((categoryTask: any)=> 
-                categoryTask.toLowerCase() !== category.toLowerCase()
-            )
-            return {
-                ...state,
-                categories: newCategories
-            }
-        }else{
-            return {
-                ...state,
-            }
-        }
-    }),
-    
-    //switch edit task dialog box state
-    on(editTaskDialogState, (state: any, action)=>{
+  // refresh category after deleting task... note runs after deletion function
+  on(deleteCategorySearch, (state: any, action) => {
+    const category = action.deleteCategory;
+    // check for remaining deletion categories
+    const checkRemaining = state.tasks.some(
+      (task: any) => task.category.toLowerCase() === category.toLowerCase()
+    );
+    let newCategories = [];
+    if (!checkRemaining) {
+      newCategories = state.categories.filter(
+        (categoryTask: any) =>
+          categoryTask.toLowerCase() !== category.toLowerCase()
+      );
+      return {
+        ...state,
+        categories: newCategories,
+      };
+    } else {
+      return {
+        ...state,
+      };
+    }
+  }),
+
+  //switch edit task dialog box state
+  on(editTaskDialogState, (state: any, action) => {
+    return {
+      ...state,
+      editId: action.editStateId,
+    };
+  }),
+  // editing the task
+  on(editTask, (state: any, action) => {
+    const newContent = action.newContent;
+    const editId = action.updateId;
+    const editedArray = state.tasks.map((singleTask: any) => {
+      // if the id is same then the object is edited
+      if (singleTask.id === editId) {
         return {
-            ...state,
-            editId: action.editStateId
-        }
-    }),
-    // editing the task
-    on(editTask, (state: any, action)=>{
-        const newContent = action.newContent;
-        const editId = action.updateId;
-        const editedArray = state.tasks.map((singleTask: any)=>{
-            // if the id is same then the object is edited
-            if(singleTask.id === editId){
-                return {
-                    ...singleTask,
-                    title:newContent.title,
-                    description: newContent.description,
-                    category: newContent.category
-                }   
-            }else{
-                return singleTask;
-            }
-        });
-        // update the categories after editing the task
-        const categories: any = [];
-        editedArray.map((task: any)=> categories.push(task.category));
-        const uniqueCategories = [...new Set([...categories])];
-        return {
-            ...state,
-            tasks: editedArray,
-            categories: uniqueCategories
-        }
-    }),
-    // updating the comment id
-    on(getCommentId, (state: any, action)=>{
-        return {
-            ...state,
-            commentId: action.commentId
-        }
-    }),
-    // adding a comment to an existing task with id as a detection
-    on(addCommentToTask, (state: any, action)=>{
-        const taskId = action.commentTaskId;
-        const comment = action.comment;
-        const commentObject = {
-            id: Date.now(),
-            commentDetails: comment
+          ...singleTask,
+          title: newContent.title,
+          description: newContent.description,
+          category: newContent.category,
         };
-        const commentedArray = state.tasks.map((singleTask: any)=>{
-            if(singleTask.id === taskId){
-                return {
-                    ...singleTask,
-                    comments: [...singleTask.comments, commentObject]
-                }
-            }else{
-                return singleTask;
-            }
-        });
+      } else {
+        return singleTask;
+      }
+    });
+    // update the categories after editing the task
+    const categories: any = [];
+    editedArray.map((task: any) => categories.push(task.category));
+    const uniqueCategories = [...new Set([...categories])];
+    return {
+      ...state,
+      tasks: editedArray,
+      categories: uniqueCategories,
+    };
+  }),
+  // updating the comment id
+  on(getCommentId, (state: any, action) => {
+    return {
+      ...state,
+      commentId: action.commentId,
+    };
+  }),
+  // adding a comment to an existing task with id as a detection
+  on(addCommentToTask, (state: any, action) => {
+    const taskId = action.commentTaskId;
+    const comment = action.comment;
+    const commentObject = {
+      id: Date.now(),
+      commentDetails: comment,
+    };
+    const commentedArray = state.tasks.map((singleTask: any) => {
+      if (singleTask.id === taskId) {
         return {
-            ...state,
-            tasks: commentedArray
-        }
-    }),
-    // deleting comment from task
-    on(deleteComment, (state: any, action)=>{
-        const deleteCommentId = action.deleteCommentId;
-        const taskId = action.taskId;
-        const afterDeletedCommentArray = state.tasks.map((task: any)=>{
-            if(task.id === taskId){
-                return {
-                    ...task,
-                    comments: task.comments.filter((comment: any)=>
-                    comment.id !== deleteCommentId)
-                }
-            }else{
-                return task;
-            }
-        });
-        return {
-            ...state,
-            tasks: afterDeletedCommentArray
-        }
-    }),
-    // toggle archive state
-    on(toggleArchiveState, (state: any, action)=>{
-        const prevState = action.archiveState;
-        return {
-            ...state,
-            archiveState: !prevState
-        }
-    }),
-    // filter tasks based on search term
-    on(filterTasks, (state: any, action)=>{
-        const searchTerm = action.searchTerm;
-        const existingTasks = [...state.tasks];
-        if(searchTerm.toLowerCase() === 'all'){
-            return {
-                ...state,
-                filterState: false,
-            }
-        }else{
-            const filteredTasks: any = existingTasks.filter((task: any)=>
-            task.category.toLowerCase().startsWith(searchTerm.toLowerCase()) || 
-            task.title.toLowerCase().startsWith(searchTerm.toLowerCase())
-        );
-        return {
-            ...state,
-            filteredTasks: filteredTasks,
-            filterState: true
-        }
-        }
-    }),
-    // switch filter state to false
-    on(switchFilterState,(state: any, action)=>{
-        return {
-            ...state,
-            filterState: false
-        }
-    }),
-    // control theme state between dark and light themes
-    on(controlThemeState, (state: any, action)=>{
-        const initalThemeState = state.themeState;
-        return {
-            ...state,
-            themeState: !initalThemeState
-        }
-    }),
-    // receive categories and tasks for distribution
-    on(passTasksByCategories, (state: any, action)=>{
-        let categories: any = action.categories;
-        let tasks = action.tasks;
-        // distributing tasks to categories
-        let categoryObject: any = {};
-        for(let i = 0; i < categories.length; i++){
-            categoryObject[categories[i].toLowerCase()] = [];
+          ...singleTask,
+          comments: [...singleTask.comments, commentObject],
         };
-        // injecting arrays 
-        for(let index in tasks){
-            const category = tasks[index].category.toLowerCase();
-            if(categoryObject[category]){
-                categoryObject[category] = [...categoryObject[category], tasks[index]];
-            }else{
-                categoryObject[category] = [];
-            }
-        }
+      } else {
+        return singleTask;
+      }
+    });
+    return {
+      ...state,
+      tasks: commentedArray,
+    };
+  }),
+  // deleting comment from task
+  on(deleteComment, (state: any, action) => {
+    const deleteCommentId = action.deleteCommentId;
+    const taskId = action.taskId;
+    const afterDeletedCommentArray = state.tasks.map((task: any) => {
+      if (task.id === taskId) {
         return {
-            ...state,
-            categoryTasks: categoryObject
-        }
-    }),
-    // turning on category view
-    on(activateCategoryView, (state: any, action)=>{
-        const categoryViewSwitch = !state.categoryView;
-        return {
-            ...state,
-           categoryView: categoryViewSwitch
-        }
-    }),
-    // always turn to default view when searching
-    on(turnToDefaultView, (state: any, action)=>{
-        return {
-            ...state,
-            categoryView: false,
-        }
-    }),
-    // upload in person task
-    on(uploadDoneTasks, (state : any)=>{
-        const newDoneTasks = state.tasks.filter((singleTask: any)=>
-            singleTask.inprogress === true
-        );
-        return {
-            ...state,
-            doneTasks: newDoneTasks,
-        }
-    })
- );
+          ...task,
+          comments: task.comments.filter(
+            (comment: any) => comment.id !== deleteCommentId
+          ),
+        };
+      } else {
+        return task;
+      }
+    });
+    return {
+      ...state,
+      tasks: afterDeletedCommentArray,
+    };
+  }),
+  // toggle archive state
+  on(toggleArchiveState, (state: any, action) => {
+    const prevState = action.archiveState;
+    return {
+      ...state,
+      archiveState: !prevState,
+    };
+  }),
+  // filter tasks based on search term
+  on(filterTasks, (state: any, action) => {
+    const searchTerm = action.searchTerm;
+    const existingTasks = [...state.tasks];
+    if (searchTerm.toLowerCase() === 'all') {
+      return {
+        ...state,
+        filterState: false,
+      };
+    } else {
+      const filteredTasks: any = existingTasks.filter(
+        (task: any) =>
+          task.category.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
+          task.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+      );
+      return {
+        ...state,
+        filteredTasks: filteredTasks,
+        filterState: true,
+      };
+    }
+  }),
+  // switch filter state to false
+  on(switchFilterState, (state: any, action) => {
+    return {
+      ...state,
+      filterState: false,
+    };
+  }),
+  // control theme state between dark and light themes
+  on(controlThemeState, (state: any, action) => {
+    const initalThemeState = state.themeState;
+    return {
+      ...state,
+      themeState: !initalThemeState,
+    };
+  }),
+  // receive categories and tasks for distribution
+  on(passTasksByCategories, (state: any, action) => {
+    let categories: any = action.categories;
+    let tasks = action.tasks;
+    // distributing tasks to categories
+    let categoryObject: any = {};
+    for (let i = 0; i < categories.length; i++) {
+      categoryObject[categories[i].toLowerCase()] = [];
+    }
+    // injecting arrays
+    for (let index in tasks) {
+      const category = tasks[index].category.toLowerCase();
+      if (categoryObject[category]) {
+        categoryObject[category] = [...categoryObject[category], tasks[index]];
+      } else {
+        categoryObject[category] = [];
+      }
+    }
+    return {
+      ...state,
+      categoryTasks: categoryObject,
+    };
+  }),
+  // turning on category view
+  on(activateCategoryView, (state: any, action) => {
+    const categoryViewSwitch = !state.categoryView;
+    return {
+      ...state,
+      categoryView: categoryViewSwitch,
+    };
+  }),
+  // always turn to default view when searching
+  on(turnToDefaultView, (state: any, action) => {
+    return {
+      ...state,
+      categoryView: false,
+    };
+  }),
+  // upload in person task
+  on(uploadDoneTasks, (state: any) => {
+    const newDoneTasks = state.tasks.filter(
+      (singleTask: any) => singleTask.inprogress === true
+    );
+    return {
+      ...state,
+      doneTasks: newDoneTasks,
+    };
+  })
+);
 
 // returning the primary reducer as a callable value
-export function taskReducer(state: any, action: any){
-    return task_reducer(state, action);
+export function taskReducer(state: any, action: any) {
+  return task_reducer(state, action);
 }
-
